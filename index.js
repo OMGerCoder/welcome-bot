833481cde73f0e4b8446bc93d7a4d3fb0b443e71
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-10 11:47:27 -0400

    added command to link channel to database

--- index.js
+++ index.js
@@ -22,4 +22,19 @@ bot.on('ready', () =>{
     console.log('Ready');
     guildWelcomeChannels.sync();
 })
+bot.on('message', msg =>{
+    if(!msg.content.startsWith('+') || msg.author.bot) return;
+    const args = msg.content.slice(1).split(' ');
+    const cmd = args.shift().toLowerCase()
+    if(cmd === 'linkchannel') {
+        const channel = msg.mentions.channels.first() || msg.guild.channels.cache.get(args[0]);
+        if(!channel) return msg.channel.send('Invalid channel.')
+        
+        guildWelcomeChannels.findOne({where: {guild_id: msg.guild.id}}).then((obj) =>{
+            if(obj) return msg.channel.send('You already have the channel in the database!')
+            guildWelcomeChannels.create({ guild_id: msg.guild.id, channel_id: channel.id })
+            return msg.channel.send('Done!');
+        })
+    }
+})
 bot.login(process.env.TOKEN)
