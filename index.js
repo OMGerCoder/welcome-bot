eb834b276252fe07cec0e6727907c91c7a06a01e
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-10 11:11:34 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client } = require('discord.js');
+const bot = new Client();
+const Sequelize = require('sequelize');
+const db = new Sequelize('db', 'user', 'password', {
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite'
+})
+
+const guildWelcomeChannels = db.define('guild_welcome_channels', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    guild_id: Sequelize.STRING,
+    channel_id: Sequelize.STRING
+})
+bot.on('ready', () =>{
+    console.log('Ready');
+    guildWelcomeChannels.sync();
+})
